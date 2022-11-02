0d703697f5d05c63861ccd9569624fa5883cc338
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2022-11-02 17:25:52 +0100

    Ajout Express et MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,27 @@
+// appel de Express pour l'api 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://Pierrick:piquanteOPC@cluster0.g6cnyan.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// mettre la requête CORS
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+
+
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
